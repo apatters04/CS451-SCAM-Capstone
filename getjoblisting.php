a7355e912b19af7b2db649b4d5e2a6b3378d6918
Christian Caldwell <caldwellchristian2@gmail.com>
2023-11-03 17:36:14 -0500

    filter working now on job availability form

---
+++ getjoblisting.php
@@ -0,0 +1,53 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "", "cs451r");
+if($mysqli->connect_error) {
+    exit('Could not connect');
+}
+
+$jobType = isset($_GET['jobType']) ? $_GET['jobType'] : "";
+$department = isset($_GET['courseCode']) ? $_GET['courseCode'] : "";
+$day = isset($_GET['day']) ? $_GET['day'] : "";
+
+$sql = "SELECT jobType, courseCode, courseInstructor, courseDays, courseTime FROM activejobs WHERE 1 ";
+
+if (!empty($jobType)) {
+    $sql .= "AND jobType = '$jobType' ";
+}
+
+if (!empty($department)) {
+    $sql .= "AND courseCode LIKE '%$department%' ";
+}
+
+if (!empty($day)) {
+    $sql .= "AND courseDays LIKE '%$day%' ";
+}
+
+$result = $mysqli->query($sql);
+
+if ($result) {
+    echo "<table class='table table-hover'>
+            <tr>
+                <th scope='col'>Job Type</th>
+                <th>Course</th>
+                <th>Instructor</th>
+                <th>Day</th>
+                <th>Time</th>
+            </tr>";
+
+    while($row = $result->fetch_assoc()) {
+        echo "<tr>
+                <td>" . $row["jobType"]. "</td>
+                <td>" . $row["courseCode"]. "</td>
+                <td>" . $row["courseInstructor"]. "</td>
+                <td>" . $row["courseDays"]. "</td>
+                <td>" . $row["courseTime"]. "</td>
+            </tr>";
+    }
+
+    echo "</table>";
+} else {
+    echo "Error in query execution: " . $mysqli->error;
+}
+
+$mysqli->close();
+?>
--- joblistings.php
+++ joblistings.php
@@ -1,110 +1,68 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Applications</title>
+    <title>Job Listings</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
+
 <body>
-    <div class="header">        
-        <h1>CSEE GTA Application</h1>
-        <ul>
-            <li><a href="Homepage.html">Homepage</a></li>            
-            <li><a href="joblistings.php">Job Availability</a></li>
-            <li><a href="Projectv1.html">Application</a></li>
-            <li><a href="Login.html">Admin Login</a></li>
-        </ul>
-    </div>
     <div class="container">
+        <form>
+            <label for="jobType">Job Type:</label>
+            <select name="jobType" id="jobType">
+                <option value="">Both</option>
+                <option value="Grader">Grader</option>
+                <option value="Lab Instructor">Lab Instructor</option>
+            </select>
+            <label for="courseCode">Department:</label>
+            <select name="courseCode" id="courseCode">
+                <option value="">All</option>
+                <option value="CS">CS</option>
+                <option value="IT">IT</option>
+                <option value="ECE">ECE</option>
+                <option value="BSCS">BSCS</option>
+            </select>
+            <label for="day">Day:</label>
+            <select name="day" id="day">
+                <option value="">All</option>
+                <option value="M">Monday</option>
+                <option value="T">Tuesday</option>
+                <option value="W">Wednesday</option>
+                <option value="Th">Thursday</option>
+                <option value="F">Friday</option>
+            </select>
+        </form>
+        <br>
+        <div id="jobListings">Job listings will be listed here...</div>
+    </div>
 
-        
-    <form id=joblistingsForm>
-        <label for="job" >Job Type:</label>
-        <select name="job" id="levelSelect">
-            <option value="">Both</option>
-            <option value="Grader">Grader</option>
-            <option value="Lab Instructor">Lab Instructor</option>
-        </select>
-        <label for="degree">Department:</label>
-        <select name="degree" id="degreeSelect">
-            <option value="">All</option>
-            <option value="CS">CS</option>
-            <option value="IT">IT</option>
-            <option value="ECE">ECE</option>
-            <option value="BSCS">BSCS</option>
-        </select>
-        <label for="day">Day:</label>
-        <select name="day" id="day">
-            <option value="">All</option>
-            <option value="M">Monday</option>
-            <option value="T">Tuesday</option>
-            <option value="W">Wednesday</option>
-            <option value="T">Thursday</option>
-            <option value="F">Friday</option>
-        </select>
-    </form>
-    <br>
-    <div id="txtHint">Available job info will be listed here...</div>
+    <script>
+        function loadData() {
+            const jobType = document.getElementById("jobType").value;
+            const courseCode = document.getElementById("courseCode").value;
+            const day = document.getElementById("day").value;
 
-    <?php
-        $servername = "localhost";
-        $username = "root"; 
-        $password = ""; 
-        $dbname = "cs451r";
+            const xhttp = new XMLHttpRequest();
 
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-
-
-            $sql = "SELECT * FROM activejobs"; //connecting to active jobs table?
-            $result = $conn->query($sql);
-
-
-
-            if ($result->num_rows > 0) {
-                // Output table headers
-                echo "<table class='table table-hover' id='myTable'>
-                        <tr>
-                            <th scope='col'>Job Type</th>
-                            <th>Course</th>
-                            <th>Instructor</th>
-                            <th>Day</th>
-                            <th>Time</th>
-                        </tr>";
-
-                // Output data from rows
-                while($row = $result->fetch_assoc()) {
-                    echo "<tr>
-                            <td>" . $row["jobType"]. "</td>
-                            <td>" . $row["courseCode"]. "</td>
-                            <td>" . $row["courseInstructor"]. "</td>
-                            <td>" . $row["courseDays"]. "</td>
-                            <td>" . $row["courseTime"]. "</td>
-                        </tr>";
-                }
-
-                // Close the table
-                echo "</table>";
-            } else {
-                echo "0 results";
+            xhttp.onload = function() {
+                document.getElementById("jobListings").innerHTML = this.responseText;
             }
 
+            xhttp.open("GET", `getjoblisting.php?jobType=${jobType}&courseCode=${courseCode}&day=${day}`);
+            xhttp.send();
+        }
 
-        $conn->close();
-        ?>
-        
-    </div>
+        document.addEventListener("DOMContentLoaded", function() {
+            loadData();
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-	
-    
+            document.getElementById("jobType").addEventListener("change", loadData);
+            document.getElementById("courseCode").addEventListener("change", loadData);
+            document.getElementById("day").addEventListener("change", loadData);
+        });
+    </script>
 </body>
+
 </html>
