3d416aeb75168e63cd1e4d2b180489f9e79c3cf6
Christian Caldwell <caldwellchristian2@gmail.com>
2023-11-03 15:12:40 -0500

    update to the filter & post login

---
+++ getApplication.php
@@ -0,0 +1,87 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "", "cs451r");
+if($mysqli->connect_error) {
+  exit('Could not connect');
+}
+#git test 
+$level = isset($_GET['level']) ? $_GET['level'] : "";
+$major = isset($_GET['major']) ? $_GET['major'] : "";
+
+$sql = "SELECT firstName, lastName, studentID, email, phoneNumber, currentLevel, GPA, degree, graduatingSemester,
+ graduatingYear, hoursCompleted, applyingJob, internationalStudentsCheckbox, GTACert, description, serveInstructor  FROM application WHERE 1 ";
+
+if (!empty($level) && !empty($major)) {
+        $sql .= "AND currentLevel = ? AND degree = ?";
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param("ss", $level, $major);
+} elseif (!empty($level)) {
+    $sql .= "AND currentLevel = ?";
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param("s", $level);
+} elseif (!empty($major)) {
+    $sql .= "AND degree = ?";
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param("s", $major);
+} else {
+    $stmt = $mysqli->prepare($sql);
+}
+
+if ($stmt) {
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($fname, $lname, $sid, $email, $phoneNumber, $currentlevel, $gpa, $degree, $gsem, $gyear, $hcomplete, $applyjob, $istu, $gtacert, $desc, $serv);
+
+    echo "<table class='table table-hover'>";
+    echo "<tr>";
+    echo "<th>First Name</th>";
+    echo "<th>Last Name</th>";
+    echo "<th>Student ID</th>";
+    echo "<th>Email</th>";
+    echo "<th>Phone Number</th>";
+    echo "<th>Current Level</th>";
+    echo "<th>GPA</th>";
+    echo "<th>Degree</th>";
+    echo "<th>Graduating Semester</th>";
+    echo "<th>Graduating Year</th>";
+    echo "<th>Hours Completed</th>";
+    echo "<th>Applying Job</th>";
+    echo "<th>International Students Checkbox</th>";
+    echo "<th>GTA Certification</th>";
+    echo "<th>Description</th>";
+    echo "<th>Serve Instructor</th>";
+    echo "</tr>";
+
+    while ($stmt->fetch()) {
+        echo "<tr>";
+        echo "<td>" . $fname . "</td>";
+        echo "<td>" . $lname . "</td>";
+        echo "<td>" . $sid . "</td>";
+        echo "<td>" . $email . "</td>";
+        echo "<td>" . $phoneNumber . "</td>";
+        echo "<td>" . $currentlevel . "</td>";
+        echo "<td>" . $gpa . "</td>";
+        echo "<td>" . $degree . "</td>";
+        echo "<td>" . $gsem . "</td>";
+        echo "<td>" . $gyear . "</td>";
+        echo "<td>" . $hcomplete . "</td>";
+        echo "<td>" . $applyjob . "</td>";
+        echo "<td>" . $istu . "</td>";
+        if ($gtacert != "" && $gtacert != "null") {
+            echo "<td><a href='$gtacert' target='_blank'>View GTACert</a></td>";
+        } else {
+            echo "<td></td>";
+        }
+        echo "<td>" . $desc . "</td>";
+        echo "<td>" . $serv . "</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+
+    $stmt->close();
+} else {
+    echo "Error in query execution: " . $mysqli->error;
+}
+
+$mysqli->close();
+?>
--- postlogin.php
+++ postlogin.php
@@ -1,130 +1,64 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Applications</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	
-    <script src="filter.js"></script>
 </head>
+
 <body>
-    <h1>Applications</h1>
-	<br>
-    <label for="currentLevel">Filter by Level:</label>
-    <select id="currentLevel">
-        <option value="">All</option>
-        <option value="BS">BS</option>
-        <option value="MS">MS</option>
-		<option value="PhD">PhD</option>
-    </select>
 
-	
-	<label for="major">Filter by Major:</label>
-    <select id="major">
-        <option value="">All</option>
-        <option value="CS">CS</option>
-        <option value="IT">IT</option>
-		<option value="ECE">ECE</option>
-		<option value="EE">EE</option>
-        <!-- Add more options based on your data -->
-    </select>
+    <h2>Application List</h2>
 
-	<label for="graduatingSemester">Filter by Semester:</label>
-    <select id="graduatingSemester">
-		<option value="">All</option>
-        <option value="Fall">Fall</option>
-        <option value="Spring">Spring</option>
-    </select>
-	
-	<label for="graduatingYear">Filter by Year:</label>
-    <select id="graduatingYear">
-		<option value="">All</option>
-        <option value="2023">2023</option>
-        <option value="2024">2024</option>
-		<option value="2025">2025</option>
-		<option value="2026">2026</option>
-		<option value="2027">2027</option>
-        <option value="2028">2028</option>
-    </select>
-	
-	<br>
-	<br>
-    
-<?php
-    // Database connection
-    $servername = "localhost";
-    $username = "root"; // Change this
-    $password = ""; // Change this
-    $dbname = "cs451r";
+    <form>
+        <select name="level" id="levelSelect">
+            <option value="">Select a level:</option>
+            <option value="BS">BS</option>
+            <option value="MS">MS</option>
+            <option value="PhD">PhD</option>
+        </select>
+        <select name="degree" id="degreeSelect">
+            <option value="">Select a major:</option>
+            <option value="CS">CS</option>
+            <option value="IT">IT</option>
+            <option value="ECE">ECE</option>
+            <option value="BSCS">BSCS</option>
+        </select>
+    </form>
+    <br>
+    <div id="txtHint">Application info will be listed here...</div>
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    <script>
+        function loadData(level, major) {
+            const xhttp = new XMLHttpRequest();
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+            xhttp.onload = function() {
+                document.getElementById("txtHint").innerHTML = this.responseText;
+            }
 
-    $sql = "SELECT * FROM application";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        // Output table headers
-        echo "<table class='table table-hover'>
-                <tr>
-                    <th scope='col'>First Name</th>
-                    <th>Last Name</th>
-                    <th>Student ID</th>
-                    <th>Email</th>
-                    <th>Phone Number</th>
-                    <th>Current Level</th>
-                    <th>Graduating Semester</th>
-                    <th>Graduating Year</th>
-                    <th>GPA</th>
-                    <th>Hours Completed</th>
-                    <th>Degree</th>
-                    <th>Major</th>
-                    <th>Applying Job</th>
-                    <th>International Student</th>
-                    <th>GTACert</th>
-                    <th>Description</th>
-                    <th>Serve Instructor</th>
-                </tr>";
-
-        // Output data from rows
-        while($row = $result->fetch_assoc()) {
-            echo "<tr>
-                    <td>" . $row["firstName"]. "</td>
-                    <td>" . $row["lastName"]. "</td>
-                    <td>" . $row["studentID"]. "</td>
-                    <td>" . $row["email"]. "</td>
-                    <td>" . $row["phoneNumber"]. "</td>
-                    <td>" . $row["currentLevel"]. "</td>
-                    <td>" . $row["graduatingSemester"]. "</td>
-                    <td>" . $row["graduatingYear"]. "</td>
-                    <td>" . $row["GPA"]. "</td>
-                    <td>" . $row["hoursCompleted"]. "</td>
-                    <td>" . $row["degree"]. "</td>
-                    <td>" . $row["major"]. "</td>
-                    <td>" . $row["applyingJob"]. "</td>
-                    <td>" . ($row["internationalStudentsCheckbox"] == 1 ? 'Yes' : 'No') . "</td>
-                    <td>" . $row["GTACert"]. "</td>
-                    <td>" . $row["description"]. "</td>
-                    <td>" . $row["serveInstructor"]. "</td>
-                </tr>";
+            xhttp.open("GET", "getapplication.php?level=" + level + "&major=" + major);
+            xhttp.send();
         }
 
-        // Close the table
-        echo "</table>";
-    } else {
-        echo "0 results";
-    }
+        document.addEventListener("DOMContentLoaded", function() {
+            loadData('', '');
+            
+            document.getElementById("levelSelect").addEventListener("change", function() {
+                var level = this.value;
+                var major = document.getElementById("degreeSelect").value; // Changed to "degreeSelect"
+                loadData(level, major);
+            });
 
-    // Close the database connection
-    $conn->close();
-?>
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        
+            document.getElementById("degreeSelect").addEventListener("change", function() { // Changed to "degreeSelect"
+                var level = document.getElementById("levelSelect").value;
+                var major = this.value;
+                loadData(level, major);
+            });
+        });
+    </script>
+    #git test 
 </body>
+
 </html>
