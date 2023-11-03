65da797f2966788e9373875a4495f37ebd1c819b
Christian Caldwell <caldwellchristian2@gmail.com>
2023-11-03 17:42:15 -0500

    update date format

--- getjoblisting.php
+++ getjoblisting.php
@@ -34,15 +34,27 @@ if ($result) {
                 <th>Time</th>
             </tr>";
 
-    while($row = $result->fetch_assoc()) {
-        echo "<tr>
-                <td>" . $row["jobType"]. "</td>
-                <td>" . $row["courseCode"]. "</td>
-                <td>" . $row["courseInstructor"]. "</td>
-                <td>" . $row["courseDays"]. "</td>
-                <td>" . $row["courseTime"]. "</td>
-            </tr>";
-    }
+            while($row = $result->fetch_assoc()) {
+                let courseTime = formatTime($row["courseTime"]);
+                console.log(courseTime);
+            
+                let newRow = "<tr>" +
+                    "<td>" + $row["jobType"] + "</td>" +
+                    "<td>" + $row["courseCode"] + "</td>" +
+                    "<td>" + $row["courseInstructor"] + "</td>" +
+                    "<td>" + $row["courseDays"] + "</td>" +
+                    "<td>" + courseTime + "</td>" +
+                    "</tr>";
+            
+                document.getElementById("jobListings").innerHTML += newRow;
+            }
+            
+            function formatTime(time) {
+                let date = new Date("2023-01-01 " + time);
+                let options = { hour: 'numeric', minute: 'numeric', hour12: true };
+                return new Intl.DateTimeFormat('en-US', options).format(date);
+            }
+  
 
     echo "</table>";
 } else {
