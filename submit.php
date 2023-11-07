8e0564a2b086469a6d6a57986a225f7214357c9c
Christian Caldwell <caldwellchristian2@gmail.com>
2023-11-07 13:36:44 -0600

    updated resume code to work

--- submit.php
+++ submit.php
@@ -8,11 +8,12 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
+
 <body>
-    <div class="header">        
+    <div class="header">
         <h1>CSEE GTA Admin Application View</h1>
         <ul>
-            <li><a href="Homepage.html">Homepage</a></li>            
+            <li><a href="Homepage.html">Homepage</a></li>
             <li><a href="joblistings.php">Job Availability</a></li>
             <li><a href="application.html">Application</a></li>
             <li><a href="Login.html">Admin Login</a></li>
@@ -21,134 +22,105 @@
 
     <?php
 
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "CS451R";
+    // Database connection details
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "CS451R";
 
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
+    // Establishing database connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Function to handle file uploads
+    function handleFileUpload($fileInputName, $targetDir)
+    {
+        if(isset($_FILES[$fileInputName]) && $_FILES[$fileInputName]["error"] == UPLOAD_ERR_OK) {
+            $target_file = $targetDir . basename($_FILES[$fileInputName]["name"]);
+
+            $uploadOk = 1;
+
+            // Check if file already exists
+            if (file_exists($target_file)) {
+                $counter = 1;
+                while (file_exists($target_file)) {
+                    $filename = pathinfo($_FILES[$fileInputName]["name"], PATHINFO_FILENAME);
+                    $extension = pathinfo($_FILES[$fileInputName]["name"], PATHINFO_EXTENSION);
+
+                    $target_file = $targetDir . $filename . ' (' . $counter . ').' . $extension;
+
+                    $counter++;
+                }
+            }
+
+            // Check file size
+            if ($_FILES[$fileInputName]["size"] > 500000) {
+                echo "Sorry, your file is too large. ";
+                $uploadOk = 0;
+            }
+
+            // Check if $uploadOk is set to 0 by an error
+            if ($uploadOk == 0) {
+                echo "Sorry, your file was not uploaded. ";
+            } else {
+                if (move_uploaded_file($_FILES[$fileInputName]["tmp_name"], $target_file)) {
+                    echo "The file " . htmlspecialchars(basename($_FILES[$fileInputName]["name"])) . " has been uploaded. ";
+                    return $target_file; // Return the file path
+                } else {
+                    echo "Sorry, there was an error uploading your file. ";
+                }
+            }
+
         }
 
-        $target_dir = "uploads/";
-        $target_file = $target_dir . basename($_FILES["GTACert"]["name"]);
-        $uploadOk = 1;
-        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+        return null;
+    }
 
-        $counter = 1;
+    // Handle GTA Certification file upload
+    $GTACertFilePath = handleFileUpload("GTACert", "uploads/");
 
-        while (file_exists($target_file)) {
-            $filename = pathinfo($_FILES["GTACert"]["name"], PATHINFO_FILENAME);
-            $extension = pathinfo($_FILES["GTACert"]["name"], PATHINFO_EXTENSION);
+    // Handle Resume file upload
+    $resumeFilePath = handleFileUpload("resume", "resume/");
 
-            $target_file = $target_dir . $filename . ' (' . $counter . ').' . $extension;
-
-            $counter++;
-        }
-
-        if ($_FILES["GTACert"]["size"] > 500000) {
-            echo "Sorry, your file is too large. ";
-            $uploadOk = 0;
-        }
-
-        if ($uploadOk == 0) {
-            echo "Sorry, your file was not uploaded. ";
+    // Handle form submission
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Retrieve form data
+        $firstName = $_POST['firstName'];
+        $lastName = $_POST['lastName'];
+        $studentID = $_POST['studentID'];
+        $email = $_POST['email'];
+        $phoneNumber = $_POST['phoneNumber'];
+        $currentLevel = $_POST['currentLevel'];
+        $graduatingSemester = $_POST['graduatingSemester'];
+        $graduatingYear = $_POST['graduatingYear'];
+        $GPA = $_POST['GPA'];
+        $hoursCompleted = $_POST['hoursCompleted'];
+        $degree = $_POST['degree'];
+        $major = $_POST['major'];
+        $applyingJob = $_POST['applyingJob'];
+        if (isset($_POST['internationalStudentsCheckbox'])) {
+            $internationalStudentsCheckbox  = 1;
         } else {
-            if (move_uploaded_file($_FILES["GTACert"]["tmp_name"], $target_file)) {
-                echo "The file " . htmlspecialchars(basename($_FILES["GTACert"]["name"])) . " has been uploaded. ";
-                $GTACertFilePath = $target_file;
-            } else {
-                echo "Sorry, there was an error uploading your file. ";
-            }
-            
-            $GTACertFilePath = $target_file;
+            $internationalStudentsCheckbox  = 0;
         }
+        $serveInstructor = $_POST['serveInstructor'];
 
-        if ($_FILES["GTACert"]["error"] != UPLOAD_ERR_OK) {
-            echo "File upload failed with error code: " . $_FILES["GTACert"]["error"];
-        }
+        // Insert data into database
+        $sql = "INSERT INTO application (firstName, lastName, studentID, email, phoneNumber, currentLevel, graduatingSemester, graduatingYear, GPA, hoursCompleted, degree, major, applyingJob, internationalStudentsCheckbox, serveInstructor, GTACert, resume)
+        VALUES ('$firstName', '$lastName', '$studentID', '$email', '$phoneNumber', '$currentLevel', '$graduatingSemester', '$graduatingYear', '$GPA', '$hoursCompleted', '$degree', '$major', '$applyingJob', '$internationalStudentsCheckbox', '$serveInstructor', '$GTACertFilePath', '$resumeFilePath')";
 
-        #Below is resume code
-
-        $target_dir = "resume/";
-        $target_file = $target_dir . basename($_FILES["resume"]["name"]);
-        $uploadOk = 1;
-        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-
-        $counter = 1;
-
-        while (file_exists($target_file)) {
-            $filename = pathinfo($_FILES["resume"]["name"], PATHINFO_FILENAME);
-            $extension = pathinfo($_FILES["resume"]["name"], PATHINFO_EXTENSION);
-
-            $target_file = $target_dir . $filename . ' (' . $counter . ').' . $extension;
-
-            $counter++;
-        }
-
-        if ($_FILES["resume"]["size"] > 500000) {
-            echo "Sorry, your file is too large. ";
-            $uploadOk = 0;
-        }
-
-        if ($uploadOk == 0) {
-            echo "Sorry, your file was not uploaded. ";
+        if ($conn->query($sql) === true) {
+            echo "Application submitted successfully!";
         } else {
-            if (move_uploaded_file($_FILES["resume"]["tmp_name"], $target_file)) {
-                echo "The file " . htmlspecialchars(basename($_FILES["resume"]["name"])) . " has been uploaded. ";
-                $resumeFilePath = $target_file;
-            } else {
-                echo "Sorry, there was an error uploading your file. ";
-            }
-            
-            $resumeFilePath = $target_file;
+            echo "Error: " . $sql . "<br>" . $conn->error;
         }
+    }
 
-        if ($_FILES["resume"]["error"] != UPLOAD_ERR_OK) {
-            echo "File upload failed with error code: " . $_FILES["resume"]["error"];
-        }
+    // Close database connection
+    $conn->close();
 
-        #resume code done
-
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $firstName = $_POST['firstName'];
-            $lastName = $_POST['lastName'];
-            $studentID = $_POST['studentID'];
-            $email = $_POST['email'];
-            $phoneNumber = $_POST['phoneNumber'];
-            $currentLevel = $_POST['currentLevel'];
-            $graduatingSemester = $_POST['graduatingSemester'];
-            $graduatingYear = $_POST['graduatingYear'];
-            $GPA = $_POST['GPA'];
-            $hoursCompleted = $_POST['hoursCompleted'];
-            $degree = $_POST['degree'];
-            $major = $_POST['major'];
-            $applyingJob = $_POST['applyingJob'];
-            if (isset($_POST['internationalStudentsCheckbox'])) {
-                $internationalStudentsCheckbox  = 1;
-            } else {
-                $internationalStudentsCheckbox  = 0;
-            }   
-            $description = $_POST['description'];
-            $serveInstructor = $_POST['serveInstructor'];
-
-            $sql = "INSERT INTO application (firstName, lastName, studentID, email, phoneNumber, currentLevel, graduatingSemester, graduatingYear, GPA, hoursCompleted, degree, major, applyingJob, internationalStudentsCheckbox, description, serveInstructor, GTACert)
-            VALUES ('$firstName', '$lastName', '$studentID', '$email', '$phoneNumber', '$currentLevel', '$graduatingSemester', '$graduatingYear', '$GPA', '$hoursCompleted', '$degree', '$major', '$applyingJob', '$internationalStudentsCheckbox', '$description', '$serveInstructor', '$GTACertFilePath')";
-
-            if ($conn->query($sql) === true) {
-                echo "Application submitted successfully!";
-            } else {
-                echo "Error: " . $sql . "<br>" . $conn->error;
-            }
-
-            $conn->close();
-        }
     ?>
 
-
-    <br>
-    <a href="application.html" class="btn btn-primary">Go back to Job Application</a>
-
-</body>
-</html>
+   
